b6c7fe923f365e12bda3f0a445d3f39bd8a7e238
patelavtx <apatel@aviatrix.com>
2023-07-14 14:29:39 +0100

    backend update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "atulrg-opstesting"
-    storage_account_name = "atulstorspk13"
+    resource_group_name  = "atulrg-ado"
+    storage_account_name = "atulado"
     container_name       = "ctrspk13-kvgh"
     key                  = "spk13-kvgh.tfstate"
   }
