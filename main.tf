3dfb503eaa972d064aba40409f1ee88aab6596fd
patelavtx <apatel@aviatrix.com>
2023-07-14 12:22:33 +0100

    updated spoke module

--- main.tf
+++ main.tf
@@ -3,25 +3,29 @@
 
 # https://registry.terraform.io/modules/terraform-aviatrix-modules/mc-overlap-nat-spoke/aviatrix/latest
 # https://registry.terraform.io/modules/terraform-aviatrix-modules/mc-spoke/aviatrix/latest
-/*
+
 module "spoke_azure_1" {
   source         = "terraform-aviatrix-modules/mc-spoke/aviatrix"
-  version        = "1.5.0"
-  cloud          = "Azure" # added for new mod
+  version        = "1.6.0"
+  cloud          = "Azure" 
   transit_gw     = var.transit_gw
   attached       = var.attached
   cidr           = var.cidr
   region         = var.region
   ha_gw          = var.ha_gw
   account        = var.account
-  resource_group = "atulrg-spoke13kvgh"
+  insane_mode    = "false"
+  #enable_bgp     = "true"
+  #local_as_number = "65013"
+  #  name of existing RG
+  resource_group = "atulrg-spoke13"
   name = var.name
   subnet_pairs = "2"
-  #included_advertised_spoke_routes = "10.255.13.1/32,10.255.13.2/32,10.255.13.251/32,10.255.13.252/32"
-
-  #tags = var.tags 
+  tags = var.tags
 }
-*/
+
+
+
 /*
 # Add delay to allow spoke-transit tunnels to come up before deploying custom nat
 resource "time_sleep" "wait_90_seconds" {
